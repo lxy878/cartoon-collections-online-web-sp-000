f8c6057672c07599d2883526ed2cff4cad444905
Xiaoyi Liu <lxy878@gmail.com>
2020-03-05 14:56:11 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index {|name, index| puts "#{index+1} #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)# code an argument here
   # Your code here
+  collections = []
+  veggies.each do |veggy|
+    collections << veggy.capitalize()+"!"
+  end
+  collections
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls_long)# code an argument here
   # Your code here
+  return calls_long.any? {|call| call.length>4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.each do |item|
+    if cheese_types.include?(item)
+      return item
+    end
+  end
+  nil
 end
